2e6b804315d540531be5b438fb88b14d91a4c9fd
Ajay Mourya <ajaymourya8553@gmail.com>
2021-07-28 23:19:41 +0530

    Renamed files

--- app/src/main/java/com/ajaymourya/android/ui/main/HomeFragment.kt
+++ app/src/main/java/com/ajaymourya/android/ui/main/HomeFragment.kt
@@ -8,24 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.ajaymourya.android.R
 
-class MainFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance() = HomeFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
